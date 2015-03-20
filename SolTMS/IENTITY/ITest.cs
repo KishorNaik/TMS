adfd0b61c87b6deb26e590b030c1837f054b3db2
KishorNaik <kishor.naik011.net@gmail.com>
2015-03-20 16:34:26 +0530

    Test - 20.03.2015

---
+++ SolTMS/IENTITY/ITest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IEntity
+{
+    public interface ITest
+    {
+        String Test { get; set; }
+    }
+}
